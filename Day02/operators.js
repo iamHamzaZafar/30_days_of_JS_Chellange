914981e1c442928c83c1057e670235c5b4656814
Hamza Zafar <link2hamzazafar@gmail.com>
2024-07-16 14:38:17 +0500

    day 2 chellange

---
+++ Day02/operators.js
@@ -0,0 +1,94 @@
+// Activity 1: rithmatic Operations
+// Task 1: Program to add to numbers ;
+const num1 = 10;
+const num2 = 5;
+const sum = num1 + num2;
+console.log(`Sum of ${num1} and ${num2} is: ${sum}`);
+
+// Task 2: Program to subtract to numbers ;
+const num3 = 15;
+const num4 = 7;
+const difference = num3 - num4;
+console.log(`Difference of ${num3} and ${num4} is: ${difference}`);
+
+// Task 3: Program to multiply to numbers ;
+
+const num5 = 4;
+const num6 = 6;
+const product = num5 * num6;
+console.log(`Product of ${num5} and ${num6} is: ${product}`);
+
+// Task 4: Program to divide to numbers ;
+
+const num7 = 18;
+const num8 = 6;
+const quotient = num7 / num8;
+console.log(`Quotient of ${num7} and ${num8} is: ${quotient}`);
+
+// Task 5: Program to find remainder
+
+const num9 = 13;
+const num10 = 4;
+const remainder = num9 % num10;
+console.log(`Remainder of ${num9} and ${num10} is: ${remainder}`);
+
+// Activity 2: Assignment Operators
+// Task 6: user += operator
+
+let a = 5;
+a += 3;
+console.log(`Value of a after += 3: ${a}`);
+
+// Task 7: user -= operator
+
+let b = 10;
+b -= 2;
+console.log(`Value of b after -= 2: ${b}`);
+
+// Activity 3: Comparison operators
+// Task 8: compare numberes using > and < 
+
+const num11 = 10;
+const num12 = 5;
+console.log(`Is ${num11} greater than ${num12}? ${num11 > num12}`);
+
+// Task 9: compare numbers using >= and <=
+
+const num13 = 10;
+const num14 = 10;
+console.log(`Is ${num13} greater than or equal to ${num14}? ${num13 >= num14}`);
+
+// Task 10: compare numbers using == and ===
+
+const num15 = 10;
+const num16 = '10';
+console.log(`Is ${num15} equal to ${num16}? ${num15 == num16}`);
+
+// Task 10: compare numbers using ===
+
+const num17 = 10;
+const num18 = '10';
+console.log(`Is ${num17} strictly equal to ${num18}? ${num17 === num18}`);
+
+// Activity 4: Logical operators
+// Task 11: program that uses && operator to combine two conditions
+const num19 = true;
+const number20 = false ;
+console.log(num19 && number20) ;
+
+// Task 12: program that uses || operator to combine two conditions
+console.log(num19 || number20)
+
+// Task 13: program that uses ! not operator 
+console.log(!number20) ;
+
+
+
+// Activity 5 : Ternary operator
+// Task 14 : program to check number is +ve or -ve using ternary operator
+
+const num21 = -1;
+let result = (num21 > 0) ? `${num21} is Positive` : `${num21} is Negative `;
+console.log(result) ;
+
+
