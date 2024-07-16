5bc241ae701589fe21c22c51529d43db187d1754
Hamza Zafar <link2hamzazafar@gmail.com>
2024-07-16 14:04:20 +0500

    day 1 chellanges completed

---
+++ Day01/Variables_DataTypes.js
@@ -0,0 +1,51 @@
+// Activity 1
+
+// Task 1 : declare a variable using var assign number and console
+
+var number = 10 ;
+console.log(number) ;
+
+
+// Task 2 : declare a variable using let assign String and console
+let name = "hamza Zafar" ;
+console.log(name) ;
+
+// Activity 2
+// Task 3 : declare a variable using const assign boolean and console
+
+const flag = true ;
+console.log(flag) ;
+
+// Activity 3
+// Task 4 : create a variable of diff types and check type of
+
+let age = 22 ;
+let designation = "junior Dev" ;
+let marital_status  = true ;
+let person = {
+    name : "John Doe",
+    age : 30,
+    city : "New York"
+}
+
+let peoples =["John", "Alex", "John", "John "] ;
+
+
+console.log(typeof designation)
+console.log(typeof age)
+console.log(typeof marital_status)
+console.log(typeof person)
+console.log(typeof peoples)
+
+
+// Activity-4
+// Re-Assigning Values
+
+let compnay = "DevLogics" ;
+
+compnay = "Devsinc" ;
+console.log(compnay) ;
+
+const role = "Associate" ;
+role = "Senior Developer" ;
+console.log(role) ; // error message assignment to the constant variable
