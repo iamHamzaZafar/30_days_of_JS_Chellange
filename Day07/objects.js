b8470ef06838da8491c27c193fc9eeb6b2188120
Hamza Zafar <link2hamzazafar@gmail.com>
2024-07-21 22:07:13 +0500

    day 7

---
+++ Day07/objects.js
@@ -0,0 +1,18 @@
+// Activity 1 : object creation and access
+//Task 1: create object representing a book with properties like title , author , year ;
+const book = {
+    title : "Atomic Habit" ,
+    author : "Stephen Covey" ,
+    year : 2019
+    fub : ()
+}
+
+// Task 2: access the book title and author ;
+console.log(book) ;
+console.log(book.title , book.author) ;
+
+// Activity 2 : object methods 
+
+// Task 3: create a method in the book object to change the author's name ;
+
+console.log(book) ; 
